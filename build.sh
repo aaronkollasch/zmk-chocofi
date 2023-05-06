2828bd0217a7dc77472013add565417f6f85ae42
Aaron Kollasch <aaron@kollasch.dev>
2023-05-06 01:25:26 -0400

    Add build script
    Requires zmk and zmk-chocofi in ~/opt directory

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+[ -d /Volumes ] && mountpath="/Volumes/NICENANO" || mountpath="/media/$USER/NICENANO"
+cd ~/opt/zmk/app
+../venv/bin/west build -p -d build/left -b nice_nano_v2 -- -DZMK_CONFIG=../../zmk-chocofi/config -DSHIELD=corne_left
+cp build/left/zephyr/zmk.uf2 build/zmk_left.uf2
+echo "copy to left board? y/n/q"
+read response
+case "$response" in
+    y)
+        cp build/left/zephyr/zmk.uf2 "$mountpath"
+        ;;
+    q)
+        exit
+        ;;
+esac
+../venv/bin/west build -p -d build/right -b nice_nano_v2 -- -DZMK_CONFIG=../../zmk-chocofi/config -DSHIELD=corne_right
+cp build/right/zephyr/zmk.uf2 build/zmk_right.uf2
+echo "copy to right board? y/n/q"
+read response
+case "$response" in
+    y)
+        cp build/right/zephyr/zmk.uf2 "$mountpath"
+        ;;
+    q)
+        exit
+        ;;
+esac
